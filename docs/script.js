6f254f7aaa411d5cb90285b5ec74152dd62f7d0b
Leo Giovanetti <hello@leog.me>
2021-11-24 15:49:10 +0000

    Init

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,19 @@
-
+const button = document.getElementById("boton");
+button.onclick = function(e) {
+    e.preventDefault();
+    fetch("https://PW2021-APINode-leog.leog4.repl.co/enviar-formulario", {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        })
+    })
+    .then(data => {
+        console.log("Data", data);
+        console.log("Cookies", document.cookie)
+    });
+}
